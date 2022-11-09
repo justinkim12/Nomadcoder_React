12f6009713dd2b4817af4021a789beb89e82b3a9
kim <justinkim1201@gmai.com>
2022-11-10 03:26:10 +0900

    final1

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,38 +1,42 @@
-import { useState,useEffect } from "react";
-import {useParams} from "react-router-dom";
-import Movie from "../components/Movie";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-function Detail(){
-    const {id} = useParams();
-    const [movie,setMovies]=useState([]);
-    const getMovie = async()=>{
+function Detail() {
+    const { id } = useParams();
+    const [loading, setLoading] = useState(true);
+    const [detail, setDetail] = useState([]);
 
-        const json = await(
-            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-        ).json();
-        
-        setMovies(json.data);
+    useEffect(() => {
+        const getDetail = async () => {
+            const json = await (
+                await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+            ).json();
+            setDetail(json.data.movie);
+            setLoading(false);
+            console.log(json.data.movie);
+        };
+        getDetail();
+    }, [id]);
 
-    };
-
-    useEffect(()=>{
-        getMovie();
-    },[]);
-
-    return (<div>
-                <img src={movie.movie.medium_cover_image} alt={movie.movie.title}/>
-                <h2>{movie.movie.title}</h2>    
-                <p>{movie.movie.description_full}</p>
-                <ul>
-                  {movie.movie.genres.map((g)=>
-                  <li key={g}>{g}</li>
-                  )}
-                </ul>    
-
-      
-    
-  
-  </div>);
-}
+    return (
+        <div>
+            {loading ? (
+                <h1> Loading...</h1 >
+            ) : (
+                <div>
+                    <img src={detail.large_cover_image} />
+                    <h1>{detail.title}</h1>
+                    <span>{detail.rating} {detail.runtime}</span>
+                    <p>{detail.description_full}</p>
+                    <ul>
+                        {detail.torrents.map((torrent) => (
+                            <li key={torrent.hash}>{torrent.url}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default Detail;
